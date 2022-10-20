b33cae5bff96237fb5fc72e130c941e4432b8afe
邵宗耀 <shaozongyao@qq.com>
2022-10-20 18:30:27 +0800

    ✨ feat: test component

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-import Vue3AutoScroll from "./packages/vue3-auto-scroll";
-// import Vue3AutoScroll from "vue3-auto-scroll";
-// import "vue3-auto-scroll/dist/style.css";
+// import Vue3AutoScroll from "./packages/vue3-auto-scroll";
+import Vue3AutoScroll from "vue3-auto-scroll";
+import "vue3-auto-scroll/dist/style.css";
 
 const app = createApp(App);
 app.use(Vue3AutoScroll);
